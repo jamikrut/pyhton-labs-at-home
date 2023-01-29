269e6ad60fa1e15978be845e6610e54782b82d07
jamikrut <jamikrut@student.edu.wszib.pl>
2023-01-29 19:20:24 +0100

    Załadowano zadanie 3 z laboratorium 1

---
+++ lab1/lab1_z3.py
@@ -0,0 +1,8 @@
+# Skrypt wyświetla na ekranie poniższy tekst:
+'''
+Cześć, mam na imię <tu wstaw swoje imię>!
+Obchodzę imieniny w <tu wstaw nazwę miesiąca>.
+'''
+
+print("Cześć, mam na imię Kuba!")
+print("Obchodzę imieniny w lipcu.")
