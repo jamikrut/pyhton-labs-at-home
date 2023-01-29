eecc36b1f87bc7d3faf90acb4035796d380ba494
jamikrut <jamikrut@student.edu.wszib.pl>
2023-01-29 19:35:31 +0100

    Załadowano zadania 2 i 3 z laboratorium 2

---
+++ lab2/lab2_z2.py
@@ -0,0 +1,8 @@
+# Skrypt zawieraja tylko jedną instrukcję print(), która wyświetli na ekranie poniższy tekst:
+'''
+Te trzy linie tekstu
+zostały wyświetlone
+za pomocą jednej instrukcji Pythona.
+'''
+
+print("Te trzy linie tekstu", "zostały wyświetlone", "za pomocą jednej instrukcji Pythona.", sep="\n")
---
+++ lab2/lab2_z3.py
@@ -0,0 +1,3 @@
+# Skrypt wyświetla na ekranie 100 linii o treści "Będę uważał/a na zajęciach z Pythona."
+
+print("Będę uważał/a na zajęciach z Pythona.\n" * 100, end="")
