2d1afedf452453049ee25517f343013f179da36e
jamikrut <jamikrut@student.wszib.edu.pl>
2023-02-21 12:41:34 +0100

    Dodano lab6_z3

---
+++ lab6/lab6_z3.py
@@ -0,0 +1,8 @@
+# Skrypt przy pomocy jednej instrukcji wyświetla na ekranie następującą figurę:
+'''
++-+-+-+-+-+-+-+-+-+
+| | | | | | | | | |
++-+-+-+-+-+-+-+-+-+
+'''
+
+print("+" + "-+" * 9, "|" + " |" * 9, "+" + "-+" * 9, sep="\n")
